c3246bc57aee04534488c54437319c99b8cd1bd7
Yoav Herman <yoavh@moveo.co.il>
2024-11-20 11:53:58 +0200

    remove build folder from repo

--- build/models/users.models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Users {
-    constructor(email, password) {
-        this.email = email;
-        this.password = password;
-    }
-}
-exports.default = Users;
--- build/routes/home.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const strings_1 = require("../constants/strings");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    res.send(strings_1.HELLO);
-});
-exports.default = router;
